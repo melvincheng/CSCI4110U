77fdb8f25edd3c1908a8a7702769dd8ad860ab31
melvincheng <melvin.cheng@live.com>
2016-12-14 00:12:59 -0500

    Update

--- final/viewer.cpp
+++ final/viewer.cpp
@@ -1,4 +1,4 @@
-    /************************************************************
+	/************************************************************
 *                   CSCI 4110 Lab 2
 *
 *  Basic OpenGL program that shows how to set up a
@@ -19,6 +19,7 @@
 #include "Shaders.h"
 #include <fstream>
 #include <iostream>
+#include <vector>
 
 #define CUBE
 
@@ -35,12 +36,12 @@ GLuint objVAO;
 int triangles;
 
 void init() {
-    GLuint vbuffer;
-    GLuint ibuffer;
-    GLint vPosition;
-    GLint vNormal;
+	GLuint vbuffer;
+	GLuint ibuffer;
+	GLint vPosition;
+	GLint vNormal;
 
-    int nv = 32;
+	int nv = 32;
 	int nn = 24;
 	int ni = 36;
 
@@ -50,188 +51,258 @@ void init() {
 	std::string constants;
 	std::string axiom;
 	std::string pattern;
+
+	std::vector<std::string> varVec;
+	std::vector<std::string> constVec;
+	std::vector<std::string> pattVec;
+
+
+
+
 	float angle;
 
 	std::ifstream file;
+	std::string line;
+	std::string foo = "foo";
 	file.open("input.txt");
-	file>>iter;
-	file>>var;
-	file>>constants;
-	file>>axiom;
-	file>>pattern;
-	file>>angle;
+	// while(std::getline(file,line)){
+	// 	std::cout << line << std::endl; 
+	// }
+	std::getline(file,line);
+	iter = std::stoi(line);
+	std::cout << "n = " << iter << std::endl;
+	std::getline(file, var);
+	std::cout << "Variables: " << var << std::endl;
+	std::getline(file, constants);
+	std::cout << "Constants: " << constants << std::endl;
+	std::getline(file, axiom);
+	std::cout << "Axiom: " << axiom << std::endl;
+	std::getline(file, pattern);
+	std::cout << "Patterns: " << pattern << std::endl;
+	std::getline(file,line);
+	angle = std::stof(line);
+	std::cout << "Angle: " << angle << std::endl;
 
-    glGenVertexArrays(1, &objVAO);
-    glBindVertexArray(objVAO);
 
-    GLfloat vertices[8][4] = {
-            { -1.0, -1.0, -1.0, 1.0 },      //0
-            { -1.0, -1.0, 1.0, 1.0 },       //1
-            { -1.0, 1.0, -1.0, 1.0 },       //2
-            { -1.0, 1.0, 1.0, 1.0 },        //3
-            { 1.0, -1.0, -1.0, 1.0 },       //4
-            { 1.0, -1.0, 1.0, 1.0 },        //5
-            { 1.0, 1.0, -1.0, 1.0 },        //6
-            { 1.0, 1.0, 1.0, 1.0 }          //7
-    };
+	std::string token;
+	int pos = 0;
 
-    GLfloat normals[8][3] = {
-            { -1.0, -1.0, -1.0 },           //0
-            { -1.0, -1.0, 1.0 },            //1
-            { -1.0, 1.0, -1.0 },            //2
-            { -1.0, 1.0, 1.0 },             //3
-            { 1.0, -1.0, -1.0 },            //4
-            { 1.0, -1.0, 1.0 },             //5
-            { 1.0, 1.0, -1.0 },             //6
-            { 1.0, 1.0, 1.0 }               //7
-    };
 
-    GLuint indexes[36] = { 0, 1, 3, 0, 2, 3,
-        0, 4, 5, 0, 1, 5,
-        2, 6, 7, 2, 3, 7,
-        0, 4, 6, 0, 2, 6,
-        1, 5, 7, 1, 3, 7,
-        4, 5, 7, 4, 6, 7 };
+	while ((pos = var.find(" ")) != std::string::npos){
+		token = var.substr(0, pos);
+		varVec.push_back(token);
+		var.erase(0, pos + 1);
+	}
+	varVec.push_back(var);
 
-    triangles = 12;
+	for(int i = 0; i != varVec.size(); i++){
+		std::cout << varVec[i] << " ";
+	}
+	std::cout<<std::endl;
 
-    glGenBuffers(1, &vbuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vbuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices) + sizeof(normals), NULL, GL_STATIC_DRAW);
-    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
-    glBufferSubData(GL_ARRAY_BUFFER, sizeof(vertices), sizeof(normals), normals);
+	while ((pos = constants.find(" ")) != std::string::npos){
+		token = constants.substr(0, pos);
+		constVec.push_back(token);
+		constants.erase(0, pos + 1);
+	}
+	constVec.push_back(constants);
 
-    glGenBuffers(1, &ibuffer);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibuffer);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indexes), indexes, GL_STATIC_DRAW);
+	for(int i = 0; i != constVec.size(); i++){
+		std::cout << constVec[i] << " ";
+	}
+	std::cout<<std::endl;
 
-    glUseProgram(program);
-    vPosition = glGetAttribLocation(program, "vPosition");
-    glVertexAttribPointer(vPosition, 4, GL_FLOAT, GL_FALSE, 0, 0);
-    glEnableVertexAttribArray(vPosition);
-    vNormal = glGetAttribLocation(program, "vNormal");
-    glVertexAttribPointer(vNormal, 3, GL_FLOAT, GL_FALSE, 0, (void*) sizeof(*vertices));
-    glEnableVertexAttribArray(vNormal);
+	while ((pos = pattern.find(" ")) != std::string::npos){
+		token = pattern.substr(0, pos);
+		pattVec.push_back(token);
+		pattern.erase(0, pos + 1);
+	}
+	pattVec.push_back(pattern);
+
+	for(int i = 0; i != pattVec.size(); i++){
+		std::cout << pattVec[i] << " ";
+	}
+	std::cout<<std::endl;
+
+
+	for(int i = 0; i < iter; i++){
+		for(int j = 0; j < pattVec.size(); j++){
+			if()
+		}
+	}
+
+	glGenVertexArrays(1, &objVAO);
+	glBindVertexArray(objVAO);
+
+
+	GLfloat vertices[8][4] = {
+			{ -1.0, -1.0, -1.0, 1.0 },      //0
+			{ -1.0, -1.0, 1.0, 1.0 },       //1
+			{ -1.0, 1.0, -1.0, 1.0 },       //2
+			{ -1.0, 1.0, 1.0, 1.0 },        //3
+			{ 1.0, -1.0, -1.0, 1.0 },       //4
+			{ 1.0, -1.0, 1.0, 1.0 },        //5
+			{ 1.0, 1.0, -1.0, 1.0 },        //6
+			{ 1.0, 1.0, 1.0, 1.0 }          //7
+	};
+
+	GLfloat normals[8][3] = {
+			{ -1.0, -1.0, -1.0 },           //0
+			{ -1.0, -1.0, 1.0 },            //1
+			{ -1.0, 1.0, -1.0 },            //2
+			{ -1.0, 1.0, 1.0 },             //3
+			{ 1.0, -1.0, -1.0 },            //4
+			{ 1.0, -1.0, 1.0 },             //5
+			{ 1.0, 1.0, -1.0 },             //6
+			{ 1.0, 1.0, 1.0 }               //7
+	};
+
+	GLuint indexes[36] = { 0, 1, 3, 0, 2, 3,
+		0, 4, 5, 0, 1, 5,
+		2, 6, 7, 2, 3, 7,
+		0, 4, 6, 0, 2, 6,
+		1, 5, 7, 1, 3, 7,
+		4, 5, 7, 4, 6, 7 };
+
+	triangles = 12;
+
+	glGenBuffers(1, &vbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices) + sizeof(normals), NULL, GL_STATIC_DRAW);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+	glBufferSubData(GL_ARRAY_BUFFER, sizeof(vertices), sizeof(normals), normals);
+
+	glGenBuffers(1, &ibuffer);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibuffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indexes), indexes, GL_STATIC_DRAW);
+
+	glUseProgram(program);
+	vPosition = glGetAttribLocation(program, "vPosition");
+	glVertexAttribPointer(vPosition, 4, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(vPosition);
+	vNormal = glGetAttribLocation(program, "vNormal");
+	glVertexAttribPointer(vNormal, 3, GL_FLOAT, GL_FALSE, 0, (void*) sizeof(*vertices));
+	glEnableVertexAttribArray(vNormal);
 
 }
 
 
 void changeSize(int w, int h) {
 
-    // Prevent a divide by zero, when window is too short
-    // (you cant make a window of zero width).
+	// Prevent a divide by zero, when window is too short
+	// (you cant make a window of zero width).
 
-    if (h == 0)
-        h = 1;
+	if (h == 0)
+		h = 1;
 
-    float ratio = 1.0 * w / h;
+	float ratio = 1.0 * w / h;
 
-    glViewport(0, 0, w, h);
+	glViewport(0, 0, w, h);
 
-    projection = glm::perspective(45.0f, ratio, 1.0f, 100.0f);
+	projection = glm::perspective(45.0f, ratio, 1.0f, 100.0f);
 
 }
 
 void displayFunc(void) {
-    glm::mat4 view;
-    int viewLoc;
-    int projLoc;
-    int colourLoc;
-    int eyeLoc;
-    int lightLoc;
-    int materialLoc;
+	glm::mat4 view;
+	int viewLoc;
+	int projLoc;
+	int colourLoc;
+	int eyeLoc;
+	int lightLoc;
+	int materialLoc;
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glUseProgram(program);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glUseProgram(program);
 
-    view = glm::lookAt(glm::vec3(eyex, eyey, eyez), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+	view = glm::lookAt(glm::vec3(eyex, eyey, eyez), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
 
-    viewLoc = glGetUniformLocation(program, "modelView");
-    glUniformMatrix4fv(viewLoc, 1, 0, glm::value_ptr(view));
-    projLoc = glGetUniformLocation(program, "projection");
-    glUniformMatrix4fv(projLoc, 1, 0, glm::value_ptr(projection));
+	viewLoc = glGetUniformLocation(program, "modelView");
+	glUniformMatrix4fv(viewLoc, 1, 0, glm::value_ptr(view));
+	projLoc = glGetUniformLocation(program, "projection");
+	glUniformMatrix4fv(projLoc, 1, 0, glm::value_ptr(projection));
 
-    colourLoc = glGetUniformLocation(program, "colour");
-    glUniform4f(colourLoc, 1.0, 0.0, 0.0, 1.0);
-    eyeLoc = glGetUniformLocation(program, "Eye");
-    glUniform3f(eyeLoc, eyex, eyey, eyez);
-    lightLoc = glGetUniformLocation(program, "light");
-    glUniform3f(lightLoc, 1.0, 1.0, 1.0);
-    materialLoc = glGetUniformLocation(program, "material");
-    glUniform4f(materialLoc, 0.3, 0.7, 0.7, 150.0);
-    
-    glBindVertexArray(objVAO);
-    glDrawElements(GL_TRIANGLES, 3*triangles, GL_UNSIGNED_INT, NULL);
+	colourLoc = glGetUniformLocation(program, "colour");
+	glUniform4f(colourLoc, 1.0, 0.0, 0.0, 1.0);
+	eyeLoc = glGetUniformLocation(program, "Eye");
+	glUniform3f(eyeLoc, eyex, eyey, eyez);
+	lightLoc = glGetUniformLocation(program, "light");
+	glUniform3f(lightLoc, 1.0, 1.0, 1.0);
+	materialLoc = glGetUniformLocation(program, "material");
+	glUniform4f(materialLoc, 0.3, 0.7, 0.7, 150.0);
+	
+	glBindVertexArray(objVAO);
+	glDrawElements(GL_TRIANGLES, 3*triangles, GL_UNSIGNED_INT, NULL);
 
-    glutSwapBuffers();
+	glutSwapBuffers();
 }
 
 void keyboardFunc(unsigned char key, int x, int y) {
 
-    switch (key) {
-    case 'a':
-        phi -= 0.1;
-        break;
-    case 'd':
-        phi += 0.1;
-        break;
-    case 'w':
-        theta += 0.1;
-        break;
-    case 's':
-        theta -= 0.1;
-        break;
-    }
+	switch (key) {
+	case 'a':
+		phi -= 0.1;
+		break;
+	case 'd':
+		phi += 0.1;
+		break;
+	case 'w':
+		theta += 0.1;
+		break;
+	case 's':
+		theta -= 0.1;
+		break;
+	}
 
-    eyex = r*sin(theta)*cos(phi);
-    eyey = r*sin(theta)*sin(phi);
-    eyez = r*cos(theta);
+	eyex = r*sin(theta)*cos(phi);
+	eyey = r*sin(theta)*sin(phi);
+	eyez = r*cos(theta);
 
-    glutPostRedisplay();
+	glutPostRedisplay();
 
 }
 
 int main(int argc, char **argv) {
 	int fs;
-    int vs;
+	int vs;
 
-    glutInit(&argc, argv);
-    glutInitContextVersion(3, 3);
+	glutInit(&argc, argv);
+	glutInitContextVersion(3, 3);
 
 
-    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-    glutInitWindowPosition(100, 100);
-    glutInitWindowSize(320, 320);
-    glutCreateWindow("Viewer");
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(320, 320);
+	glutCreateWindow("Viewer");
 
-    glewExperimental = GL_TRUE;
-    GLenum error = glewInit();
-    if (error != GLEW_OK) {
-        printf("Error starting GLEW: %s\n", glewGetErrorString(error));
-        exit(0);
-    }
+	glewExperimental = GL_TRUE;
+	GLenum error = glewInit();
+	if (error != GLEW_OK) {
+		printf("Error starting GLEW: %s\n", glewGetErrorString(error));
+		exit(0);
+	}
 
-    glutDisplayFunc(displayFunc);
-    glutReshapeFunc(changeSize);
-    glutKeyboardFunc(keyboardFunc);
+	glutDisplayFunc(displayFunc);
+	glutReshapeFunc(changeSize);
+	glutKeyboardFunc(keyboardFunc);
 
-    glEnable(GL_DEPTH_TEST);
-    glClearColor(1.0, 1.0, 1.0, 1.0);
+	glEnable(GL_DEPTH_TEST);
+	glClearColor(1.0, 1.0, 1.0, 1.0);
 
-    vs = buildShader(GL_VERTEX_SHADER, "final.vs");
-    fs = buildShader(GL_FRAGMENT_SHADER, "final.fs");
-    program = buildProgram(vs, fs, 0);
-    dumpProgram(program, "Shader program");
-    init();
+	vs = buildShader(GL_VERTEX_SHADER, "final.vs");
+	fs = buildShader(GL_FRAGMENT_SHADER, "final.fs");
+	program = buildProgram(vs, fs, 0);
+	dumpProgram(program, "Shader program");
+	init();
 
-    eyex = 0.0;
-    eyez = 0.0;
-    eyey = 10.0;
+	eyex = 0.0;
+	eyez = 0.0;
+	eyey = 10.0;
 
-    theta = 1.5;
-    phi = 1.5;
-    r = 10.0;
+	theta = 1.5;
+	phi = 1.5;
+	r = 10.0;
 
-    glutMainLoop();
+	glutMainLoop();
 
 }
--- lab6/part2/viewer.cpp
+++ lab6/part2/viewer.cpp
@@ -1,4 +1,4 @@
-    /************************************************************
+	/************************************************************
 *                   CSCI 4110 Lab 2
 *
 *  Basic OpenGL program that shows how to set up a
@@ -44,147 +44,147 @@ int triangles;
  * from class.
  */
 void init() {
-    GLuint vbuffer;
-    GLuint ibuffer;
-    GLint vPosition;
-    GLint vNormal;
+	GLuint vbuffer;
+	GLuint ibuffer;
+	GLint vPosition;
+	GLint vNormal;
 
-    glGenVertexArrays(1, &objVAO);
-    glBindVertexArray(objVAO);
+	glGenVertexArrays(1, &objVAO);
+	glBindVertexArray(objVAO);
 
-    GLfloat vertices[8][4] = {
-            { -1.0, -1.0, -1.0, 1.0 },      //0
-            { -1.0, -1.0, 1.0, 1.0 },       //1
-            { -1.0, 1.0, -1.0, 1.0 },       //2
-            { -1.0, 1.0, 1.0, 1.0 },        //3
-            { 1.0, -1.0, -1.0, 1.0 },       //4
-            { 1.0, -1.0, 1.0, 1.0 },        //5
-            { 1.0, 1.0, -1.0, 1.0 },        //6
-            { 1.0, 1.0, 1.0, 1.0 }          //7
-    };
+	GLfloat vertices[8][4] = {
+		{ -1.0, -1.0, -1.0, 1.0 },      //0
+		{ -1.0, -1.0, 1.0, 1.0 },       //1
+		{ -1.0, 1.0, -1.0, 1.0 },       //2
+		{ -1.0, 1.0, 1.0, 1.0 },        //3
+		{ 1.0, -1.0, -1.0, 1.0 },       //4
+		{ 1.0, -1.0, 1.0, 1.0 },        //5
+		{ 1.0, 1.0, -1.0, 1.0 },        //6
+		{ 1.0, 1.0, 1.0, 1.0 }          //7
+	};
 
-    GLfloat normals[8][3] = {
-            { -1.0, -1.0, -1.0 },           //0
-            { -1.0, -1.0, 1.0 },            //1
-            { -1.0, 1.0, -1.0 },            //2
-            { -1.0, 1.0, 1.0 },             //3
-            { 1.0, -1.0, -1.0 },            //4
-            { 1.0, -1.0, 1.0 },             //5
-            { 1.0, 1.0, -1.0 },             //6
-            { 1.0, 1.0, 1.0 }               //7
-    };
+	GLfloat normals[8][3] = {
+		{ -1.0, -1.0, -1.0 },           //0
+		{ -1.0, -1.0, 1.0 },            //1
+		{ -1.0, 1.0, -1.0 },            //2
+		{ -1.0, 1.0, 1.0 },             //3
+		{ 1.0, -1.0, -1.0 },            //4
+		{ 1.0, -1.0, 1.0 },             //5
+		{ 1.0, 1.0, -1.0 },             //6
+		{ 1.0, 1.0, 1.0 }               //7
+	};
 
-    GLuint indexes[36] = { 0, 1, 3, 0, 2, 3,
-        0, 4, 5, 0, 1, 5,
-        2, 6, 7, 2, 3, 7,
-        0, 4, 6, 0, 2, 6,
-        1, 5, 7, 1, 3, 7,
-        4, 5, 7, 4, 6, 7 };
+	GLuint indexes[36] = { 0, 1, 3, 0, 2, 3,
+		0, 4, 5, 0, 1, 5,
+		2, 6, 7, 2, 3, 7,
+		0, 4, 6, 0, 2, 6,
+		1, 5, 7, 1, 3, 7,
+		4, 5, 7, 4, 6, 7 };
 
-    triangles = 12;
+	triangles = 12;
 
-    glGenBuffers(1, &vbuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vbuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices) + sizeof(normals), NULL, GL_STATIC_DRAW);
-    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
-    glBufferSubData(GL_ARRAY_BUFFER, sizeof(vertices), sizeof(normals), normals);
+	glGenBuffers(1, &vbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices) + sizeof(normals), NULL, GL_STATIC_DRAW);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+	glBufferSubData(GL_ARRAY_BUFFER, sizeof(vertices), sizeof(normals), normals);
 
-    glGenBuffers(1, &ibuffer);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibuffer);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indexes), indexes, GL_STATIC_DRAW);
+	glGenBuffers(1, &ibuffer);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibuffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indexes), indexes, GL_STATIC_DRAW);
 
-    glUseProgram(program);
-    vPosition = glGetAttribLocation(program, "vPosition");
-    glVertexAttribPointer(vPosition, 4, GL_FLOAT, GL_FALSE, 0, 0);
-    glEnableVertexAttribArray(vPosition);
-    vNormal = glGetAttribLocation(program, "vNormal");
-    glVertexAttribPointer(vNormal, 3, GL_FLOAT, GL_FALSE, 0, (void*) sizeof(*vertices));
-    glEnableVertexAttribArray(vNormal);
+	glUseProgram(program);
+	vPosition = glGetAttribLocation(program, "vPosition");
+	glVertexAttribPointer(vPosition, 4, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(vPosition);
+	vNormal = glGetAttribLocation(program, "vNormal");
+	glVertexAttribPointer(vNormal, 3, GL_FLOAT, GL_FALSE, 0, (void*) sizeof(*vertices));
+	glEnableVertexAttribArray(vNormal);
 
 }
 #endif
 
 #ifdef SPHERE
 void init() {
-    GLuint vbuffer;
-    GLuint ibuffer;
-    GLint vPosition;
-    GLint vNormal;
-    GLfloat *vertices;
-    GLfloat *normals;
-    GLuint *indices;
-    std::vector<tinyobj::shape_t> shapes;
-    std::vector<tinyobj::material_t> materials;
-    int nv;
-    int nn;
-    int ni;
-    int i;
+	GLuint vbuffer;
+	GLuint ibuffer;
+	GLint vPosition;
+	GLint vNormal;
+	GLfloat *vertices;
+	GLfloat *normals;
+	GLuint *indices;
+	std::vector<tinyobj::shape_t> shapes;
+	std::vector<tinyobj::material_t> materials;
+	int nv;
+	int nn;
+	int ni;
+	int i;
 
-    glGenVertexArrays(1, &objVAO);
-    glBindVertexArray(objVAO);
+	glGenVertexArrays(1, &objVAO);
+	glBindVertexArray(objVAO);
 
-    /*  Load the obj file */
+	/*  Load the obj file */
 
-    std::string err = tinyobj::LoadObj(shapes, materials, "sphere.obj", 0);
+	std::string err = tinyobj::LoadObj(shapes, materials, "sphere.obj", 0);
 
-    if (!err.empty()) {
-        std::cerr << err << std::endl;
-        return;
-    }
+	if (!err.empty()) {
+		std::cerr << err << std::endl;
+		return;
+	}
 
-    /*  Retrieve the vertex coordinate data */
+	/*  Retrieve the vertex coordinate data */
 
-    nv = shapes[0].mesh.positions.size();
-    vertices = new GLfloat[nv];
-    for (i = 0; i<nv; i++) {
-        vertices[i] = shapes[0].mesh.positions[i];
-    }
+	nv = shapes[0].mesh.positions.size();
+	vertices = new GLfloat[nv];
+	for (i = 0; i<nv; i++) {
+		vertices[i] = shapes[0].mesh.positions[i];
+	}
 
-    /*  Retrieve the vertex normals */
+	/*  Retrieve the vertex normals */
 
-    nn = shapes[0].mesh.normals.size();
-    normals = new GLfloat[nn];
-    for (i = 0; i<nn; i++) {
-        normals[i] = shapes[0].mesh.normals[i];
-    }
+	nn = shapes[0].mesh.normals.size();
+	normals = new GLfloat[nn];
+	for (i = 0; i<nn; i++) {
+		normals[i] = shapes[0].mesh.normals[i];
+	}
 
-    /*  Retrieve the triangle indices */
+	/*  Retrieve the triangle indices */
 
-    ni = shapes[0].mesh.indices.size();
-    triangles = ni / 3;
-    indices = new GLuint[ni];
-    for (i = 0; i<ni; i++) {
-        indices[i] = shapes[0].mesh.indices[i];
-    }
+	ni = shapes[0].mesh.indices.size();
+	triangles = ni / 3;
+	indices = new GLuint[ni];
+	for (i = 0; i<ni; i++) {
+		indices[i] = shapes[0].mesh.indices[i];
+	}
 
-    /*
-    *  load the vertex coordinate data
-    */
-    glGenBuffers(1, &vbuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vbuffer);
-    glBufferData(GL_ARRAY_BUFFER, (nv + nn)*sizeof(GLfloat), NULL, GL_STATIC_DRAW);
-    glBufferSubData(GL_ARRAY_BUFFER, 0, nv*sizeof(GLfloat), vertices);
-    glBufferSubData(GL_ARRAY_BUFFER, nv*sizeof(GLfloat), nn*sizeof(GLfloat), normals);
+	/*
+	*  load the vertex coordinate data
+	*/
+	glGenBuffers(1, &vbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vbuffer);
+	glBufferData(GL_ARRAY_BUFFER, (nv + nn)*sizeof(GLfloat), NULL, GL_STATIC_DRAW);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, nv*sizeof(GLfloat), vertices);
+	glBufferSubData(GL_ARRAY_BUFFER, nv*sizeof(GLfloat), nn*sizeof(GLfloat), normals);
 
-    /*
-    *  load the vertex indexes
-    */
-    glGenBuffers(1, &ibuffer);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibuffer);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, ni*sizeof(GLuint), indices, GL_STATIC_DRAW);
+	/*
+	*  load the vertex indexes
+	*/
+	glGenBuffers(1, &ibuffer);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibuffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, ni*sizeof(GLuint), indices, GL_STATIC_DRAW);
 
-    /*
-    *  link the vertex coordinates to the vPosition
-    *  variable in the vertex program.  Do the same
-    *  for the normal vectors.
-    */
-    glUseProgram(program);
-    vPosition = glGetAttribLocation(program, "vPosition");
-    glVertexAttribPointer(vPosition, 3, GL_FLOAT, GL_FALSE, 0, 0);
-    glEnableVertexAttribArray(vPosition);
-    vNormal = glGetAttribLocation(program, "vNormal");
-    glVertexAttribPointer(vNormal, 3, GL_FLOAT, GL_FALSE, 0, (void*)(nv*sizeof(*vertices)));
-    glEnableVertexAttribArray(vNormal);
+	/*
+	*  link the vertex coordinates to the vPosition
+	*  variable in the vertex program.  Do the same
+	*  for the normal vectors.
+	*/
+	glUseProgram(program);
+	vPosition = glGetAttribLocation(program, "vPosition");
+	glVertexAttribPointer(vPosition, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(vPosition);
+	vNormal = glGetAttribLocation(program, "vNormal");
+	glVertexAttribPointer(vNormal, 3, GL_FLOAT, GL_FALSE, 0, (void*)(nv*sizeof(*vertices)));
+	glEnableVertexAttribArray(vNormal);
 
 }
 #endif
@@ -192,135 +192,135 @@ void init() {
 
 void changeSize(int w, int h) {
 
-    // Prevent a divide by zero, when window is too short
-    // (you cant make a window of zero width).
+	// Prevent a divide by zero, when window is too short
+	// (you cant make a window of zero width).
 
-    if (h == 0)
-        h = 1;
+	if (h == 0)
+		h = 1;
 
-    float ratio = 1.0 * w / h;
+	float ratio = 1.0 * w / h;
 
-    glViewport(0, 0, w, h);
+	glViewport(0, 0, w, h);
 
-    projection = glm::perspective(45.0f, ratio, 1.0f, 100.0f);
+	projection = glm::perspective(45.0f, ratio, 1.0f, 100.0f);
 
 }
 
 void displayFunc(void) {
-    glm::mat4 view;
-    int viewLoc;
-    int projLoc;
-    int colourLoc;
-    int eyeLoc;
-    int lightLoc;
-    int materialLoc;
+	glm::mat4 view;
+	int viewLoc;
+	int projLoc;
+	int colourLoc;
+	int eyeLoc;
+	int lightLoc;
+	int materialLoc;
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glUseProgram(program);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glUseProgram(program);
 
-    view = glm::lookAt(glm::vec3(eyex, eyey, eyez), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+	view = glm::lookAt(glm::vec3(eyex, eyey, eyez), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
 
-    viewLoc = glGetUniformLocation(program, "modelView");
-    glUniformMatrix4fv(viewLoc, 1, 0, glm::value_ptr(view));
-    projLoc = glGetUniformLocation(program, "projection");
-    glUniformMatrix4fv(projLoc, 1, 0, glm::value_ptr(projection));
+	viewLoc = glGetUniformLocation(program, "modelView");
+	glUniformMatrix4fv(viewLoc, 1, 0, glm::value_ptr(view));
+	projLoc = glGetUniformLocation(program, "projection");
+	glUniformMatrix4fv(projLoc, 1, 0, glm::value_ptr(projection));
 
-    colourLoc = glGetUniformLocation(program, "colour");
-    glUniform4f(colourLoc, 1.0, 0.0, 0.0, 1.0);
-    eyeLoc = glGetUniformLocation(program, "Eye");
-    glUniform3f(eyeLoc, eyex, eyey, eyez);
-    lightLoc = glGetUniformLocation(program, "light");
-    glUniform3f(lightLoc, 1.0, 1.0, 1.0);
-    materialLoc = glGetUniformLocation(program, "material");
-    glUniform4f(materialLoc, 0.3, 0.7, 0.7, 150.0);
-    
-    glBindVertexArray(objVAO);
-    glDrawElements(GL_TRIANGLES, 3*triangles, GL_UNSIGNED_INT, NULL);
+	colourLoc = glGetUniformLocation(program, "colour");
+	glUniform4f(colourLoc, 1.0, 0.0, 0.0, 1.0);
+	eyeLoc = glGetUniformLocation(program, "Eye");
+	glUniform3f(eyeLoc, eyex, eyey, eyez);
+	lightLoc = glGetUniformLocation(program, "light");
+	glUniform3f(lightLoc, 1.0, 1.0, 1.0);
+	materialLoc = glGetUniformLocation(program, "material");
+	glUniform4f(materialLoc, 0.3, 0.7, 0.7, 150.0);
+	
+	glBindVertexArray(objVAO);
+	glDrawElements(GL_TRIANGLES, 3*triangles, GL_UNSIGNED_INT, NULL);
 
-    glutSwapBuffers();
+	glutSwapBuffers();
 }
 
 void keyboardFunc(unsigned char key, int x, int y) {
 
-    switch (key) {
-    case 'a':
-        phi -= 0.1;
-        break;
-    case 'd':
-        phi += 0.1;
-        break;
-    case 'w':
-        theta += 0.1;
-        break;
-    case 's':
-        theta -= 0.1;
-        break;
-    }
+	switch (key) {
+	case 'a':
+		phi -= 0.1;
+		break;
+	case 'd':
+		phi += 0.1;
+		break;
+	case 'w':
+		theta += 0.1;
+		break;
+	case 's':
+		theta -= 0.1;
+		break;
+	}
 
-    eyex = r*sin(theta)*cos(phi);
-    eyey = r*sin(theta)*sin(phi);
-    eyez = r*cos(theta);
+	eyex = r*sin(theta)*cos(phi);
+	eyey = r*sin(theta)*sin(phi);
+	eyez = r*cos(theta);
 
-    glutPostRedisplay();
+	glutPostRedisplay();
 
 }
 
 int main(int argc, char **argv) {
-    int fs;
-    int vs;
-    int user;
+	int fs;
+	int vs;
+	int user;
 
-    char vertexName[256];
-    char fragmentName[256];
-    char *fragment;
-    char *vertex;
+	char vertexName[256];
+	char fragmentName[256];
+	char *fragment;
+	char *vertex;
 
-    glutInit(&argc, argv);
-    glutInitContextVersion(3, 3);
+	glutInit(&argc, argv);
+	glutInitContextVersion(3, 3);
 
 
-    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-    glutInitWindowPosition(100, 100);
-    glutInitWindowSize(320, 320);
-    glutCreateWindow("Viewer");
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(320, 320);
+	glutCreateWindow("Viewer");
 
-    glewExperimental = GL_TRUE;
-    GLenum error = glewInit();
-    if (error != GLEW_OK) {
-        printf("Error starting GLEW: %s\n", glewGetErrorString(error));
-        exit(0);
-    }
+	glewExperimental = GL_TRUE;
+	GLenum error = glewInit();
+	if (error != GLEW_OK) {
+		printf("Error starting GLEW: %s\n", glewGetErrorString(error));
+		exit(0);
+	}
 
-    if (argc < 2) {
-        printf("usage: viwer vertex_suffix fragment_suffix\n");
-        exit(0);
-    }
-    // vertex = argv[1];
-    fragment = argv[1];
+	if (argc < 2) {
+		printf("usage: viwer vertex_suffix fragment_suffix\n");
+		exit(0);
+	}
+	// vertex = argv[1];
+	fragment = argv[1];
 
-    glutDisplayFunc(displayFunc);
-    glutReshapeFunc(changeSize);
-    glutKeyboardFunc(keyboardFunc);
+	glutDisplayFunc(displayFunc);
+	glutReshapeFunc(changeSize);
+	glutKeyboardFunc(keyboardFunc);
 
-    glEnable(GL_DEPTH_TEST);
-    glClearColor(1.0, 1.0, 1.0, 1.0);
+	glEnable(GL_DEPTH_TEST);
+	glClearColor(1.0, 1.0, 1.0, 1.0);
 
-    sprintf(vertexName, "lab6.vs", vertex);
-    vs = buildShader(GL_VERTEX_SHADER, "lab6.vs");
-    sprintf(fragmentName, "lab6%s.fs", fragment);
-    fs = buildShader(GL_FRAGMENT_SHADER, fragmentName);
-    program = buildProgram(vs, fs, 0);
-    dumpProgram(program, "Lab 2 shader program");
-    init();
+	sprintf(vertexName, "lab6.vs", vertex);
+	vs = buildShader(GL_VERTEX_SHADER, "lab6.vs");
+	sprintf(fragmentName, "lab6%s.fs", fragment);
+	fs = buildShader(GL_FRAGMENT_SHADER, fragmentName);
+	program = buildProgram(vs, fs, 0);
+	dumpProgram(program, "Lab 2 shader program");
+	init();
 
-    eyex = 0.0;
-    eyez = 0.0;
-    eyey = 10.0;
+	eyex = 0.0;
+	eyez = 0.0;
+	eyey = 10.0;
 
-    theta = 1.5;
-    phi = 1.5;
-    r = 10.0;
+	theta = 1.5;
+	phi = 1.5;
+	r = 10.0;
 
-    glutMainLoop();
+	glutMainLoop();
 
 }
